668ba79d3763a75ce1291f3dea682fcfd481ce29
DirkFi <ming557@outlook.com>
2025-06-17 16:20:03 -0700

    Change setup and add __init__ for pip

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="async-scraper",                        # 你包在 PyPI 上的名字
-    version="0.1.0",                             
+    name="free-proxies-scraper",                        # 你包在 PyPI 上的名字
+    version="0.1.2",                             
     author="Derek Wang",
     author_email="ming557@outlook.com",
     description="An async web-scraping framework",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/DirkFi/FreeProxiesScraper",
+    license="MIT",
     packages=find_packages(where="python/src"),
     package_dir={"": "python/src"},
     python_requires=">=3.7",
-    install_requires=[                           # 填写运行时依赖
-        "beautifulsoup4"
+    install_requires=[
+        "beautifulsoup4",
         "aiohttp",
     ],
     classifiers=[
